271e8426e0851962fa9a65c750bc43fdcc364567
Talha Sattar <talhasattar.soft@gmail.com>
2023-07-03 12:13:53 +0500

    Initial commit

---
+++ app/Http/Controllers/OpenAiController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class OpenAiController extends Controller
+{
+    public function index():JsonResponse{
+
+    }
+}
